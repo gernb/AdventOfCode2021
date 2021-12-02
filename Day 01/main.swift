f98895abbaf34f06d4572eaa13ccb5267b13bd16
gernb <gern@1dot0.net>
2021-12-01 18:24:01 -0800

    Alternate solution

--- Day 01/main.swift
+++ Day 01/main.swift
@@ -13,17 +13,21 @@ enum Part1 {
     static func run(_ source: InputData) {
         let input = source.data.compactMap(Int.init)
 
-        let increaseCount = input
-            .dropFirst()
-            .enumerated()
-            .map { pair in
-                if pair.element - input[pair.offset] > 0 {
-                    return 1
-                } else {
-                    return 0
-                }
-            }
-            .reduce(0, +)
+//        let increaseCount = input
+//            .dropFirst()
+//            .enumerated()
+//            .map { pair in
+//                if pair.element - input[pair.offset] > 0 {
+//                    return 1
+//                } else {
+//                    return 0
+//                }
+//            }
+//            .reduce(0, +)
+        let increaseCount = zip(input.dropFirst(), input)
+            .filter { $0.0 > $0.1 }
+            .count
+
         print("Part 1 (\(source)): \(increaseCount)")
     }
 }
@@ -37,24 +41,31 @@ print("")
 enum Part2 {
     static func run(_ source: InputData) {
         let input = source.data.compactMap(Int.init)
-        let windowedInput = input
-            .dropLast(2)
-            .enumerated()
-            .map {
-                $0.element + input[$0.offset + 1] + input[$0.offset + 2]
-            }
 
-        let increaseCount = windowedInput
-            .dropFirst()
-            .enumerated()
-            .map { pair in
-                if pair.element - windowedInput[pair.offset] > 0 {
-                    return 1
-                } else {
-                    return 0
-                }
-            }
-            .reduce(0, +)
+//        let windowedInput = input
+//            .dropLast(2)
+//            .enumerated()
+//            .map {
+//                $0.element + input[$0.offset + 1] + input[$0.offset + 2]
+//            }
+//
+//        let increaseCount = windowedInput
+//            .dropFirst()
+//            .enumerated()
+//            .map { pair in
+//                if pair.element - windowedInput[pair.offset] > 0 {
+//                    return 1
+//                } else {
+//                    return 0
+//                }
+//            }
+//            .reduce(0, +)
+
+        let windows = zip(input, zip(input.dropFirst(1), input.dropFirst(2)))
+            .map { $0.0 + $0.1.0 + $0.1.1 }
+        let increaseCount = zip(windows.dropFirst(), windows)
+            .filter { $0.0 > $0.1 }
+            .count
 
         print("Part 2 (\(source)): \(increaseCount)")
     }

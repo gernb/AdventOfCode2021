90409f04047476eefaeac99bee5f4fac7ed0fffb
gernb <gern@1dot0.net>
2021-12-02 21:17:43 -0800

    Part 1

--- Day 03/main.swift
+++ Day 03/main.swift
@@ -11,9 +11,36 @@ print("Day 03:")
 
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let input = source.data.map { Array($0) }
 
-        print("Part 1 (\(source)):")
+        var gammaRateArray = [String]()
+        var epsilonRateArray = [String]()
+
+        let bitCount = input[0].count
+        for bit in 0 ..< bitCount {
+            var zeroCount = 0
+            var oneCount = 0
+            input.forEach {
+                if $0[bit] == "0" {
+                    zeroCount += 1
+                } else {
+                    oneCount += 1
+                }
+            }
+            if zeroCount > oneCount {
+                gammaRateArray.append("0")
+                epsilonRateArray.append("1")
+            } else {
+                assert(oneCount > zeroCount)
+                gammaRateArray.append("1")
+                epsilonRateArray.append("0")
+            }
+        }
+
+        let gammaRate = Int(gammaRateArray.joined(), radix: 2)!
+        let epsilonRate = Int(epsilonRateArray.joined(), radix: 2)!
+
+        print("Part 1 (\(source)): gamma rate = \(gammaRate), epsilon rate = \(epsilonRate), answer = \(gammaRate * epsilonRate)")
     }
 }
 

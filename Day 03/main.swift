5d948f8364e62fedef3d300532147dc5ec66ae12
gernb <gern@1dot0.net>
2021-12-03 09:19:24 -0800

    Refactor solution

--- Day 03/main.swift
+++ Day 03/main.swift
@@ -9,6 +9,12 @@
 
 print("Day 03:")
 
+func countBits(at position: Int, input: [[String.Element]]) -> (zeros: Int, ones: Int) {
+    let zeroCount = input.filter { $0[position] == "0" }.count
+    let oneCount = input.count - zeroCount
+    return (zeroCount, oneCount)
+}
+
 enum Part1 {
     static func run(_ source: InputData) {
         let input = source.data.map { Array($0) }
@@ -18,20 +24,12 @@ enum Part1 {
 
         let bitCount = input[0].count
         for bit in 0 ..< bitCount {
-            var zeroCount = 0
-            var oneCount = 0
-            input.forEach {
-                if $0[bit] == "0" {
-                    zeroCount += 1
-                } else {
-                    oneCount += 1
-                }
-            }
+            let (zeroCount, oneCount) = countBits(at: bit, input: input)
             if zeroCount > oneCount {
                 gammaRateArray.append("0")
                 epsilonRateArray.append("1")
             } else {
-                assert(oneCount > zeroCount)
+                assert(oneCount != zeroCount)
                 gammaRateArray.append("1")
                 epsilonRateArray.append("0")
             }
@@ -54,47 +52,30 @@ enum Part2 {
     static func run(_ source: InputData) {
         let input = source.data.map { Array($0) }
 
+        var position = 0
         var o2Gen = input
-        var co2Scrub = input
-
-        let bitCount = input[0].count
-        for bit in 0 ..< bitCount {
-            var zeroCount = 0
-            var oneCount = 0
-            o2Gen.forEach {
-                if $0[bit] == "0" {
-                    zeroCount += 1
-                } else {
-                    oneCount += 1
-                }
-            }
+        while o2Gen.count > 1 {
+            let (zeroCount, oneCount) = countBits(at: position, input: o2Gen)
             if oneCount >= zeroCount {
-                o2Gen = o2Gen.filter { $0[bit] == "1" }
+                o2Gen = o2Gen.filter { $0[position] == "1" }
             } else {
-                o2Gen = o2Gen.filter { $0[bit] == "0" }
+                o2Gen = o2Gen.filter { $0[position] == "0" }
             }
-
-            if co2Scrub.count == 1 {
-                continue
-            }
-
-            zeroCount = 0
-            oneCount = 0
-            co2Scrub.forEach {
-                if $0[bit] == "0" {
-                    zeroCount += 1
-                } else {
-                    oneCount += 1
-                }
-            }
-            if zeroCount <= oneCount {
-                co2Scrub = co2Scrub.filter { $0[bit] == "0" }
-            } else {
-                co2Scrub = co2Scrub.filter { $0[bit] == "1" }
-            }
+            position += 1
         }
-
         let o2GenRating = Int(String(o2Gen.first!), radix: 2)!
+
+        position = 0
+        var co2Scrub = input
+        while co2Scrub.count > 1 {
+            let (zeroCount, oneCount) = countBits(at: position, input: co2Scrub)
+            if zeroCount <= oneCount {
+                co2Scrub = co2Scrub.filter { $0[position] == "0" }
+            } else {
+                co2Scrub = co2Scrub.filter { $0[position] == "1" }
+            }
+            position += 1
+        }
         let co2ScrubRating = Int(String(co2Scrub.first!), radix: 2)!
 
         print("Part 2 (\(source)): oxygen generator rating = \(o2GenRating), CO2 scrubber rating = \(co2ScrubRating), answer = \(o2GenRating * co2ScrubRating)")

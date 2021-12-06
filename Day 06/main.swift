a9e86ed8a87a240f3e454eeb6ae459e3354a9bc8
peter bohac <gern@1dot0.net>
2021-12-05 21:45:42 -0800

    Part 2

--- Day 06/main.swift
+++ Day 06/main.swift
@@ -38,9 +38,27 @@ print("")
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let fish = source.data
 
-        print("Part 2 (\(source)):")
+        var counts = Array(repeating: 0, count: 9)
+        for f in fish {
+            counts[f] += 1
+        }
+
+        for _ in 0 ..< 256 {
+            let newFish = counts[0]
+            counts[0] = counts[1]
+            counts[1] = counts[2]
+            counts[2] = counts[3]
+            counts[3] = counts[4]
+            counts[4] = counts[5]
+            counts[5] = counts[6]
+            counts[6] = counts[7] + newFish
+            counts[7] = counts[8]
+            counts[8] = newFish
+        }
+
+        print("Part 2 (\(source)): \(counts.reduce(0, +))")
     }
 }
 

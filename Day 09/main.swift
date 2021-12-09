28867dbb7a88a8920dc09c24d273752723418416
peter bohac <gern@1dot0.net>
2021-12-08 21:40:11 -0800

    Part 2

--- Day 09/main.swift
+++ Day 09/main.swift
@@ -57,11 +57,59 @@ InputData.allCases.forEach(Part1.run)
 
 print("")
 
+extension Point {
+    var left: Point { Point(x: x-1, y: y) }
+    var right: Point { Point(x: x+1, y: y) }
+    var up: Point { Point(x: x, y: y-1) }
+    var down: Point { Point(x: x, y: y+1) }
+}
+
 enum Part2 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let input = source.data.map { line in
+            Array(line).map(String.init).compactMap(Int.init)
+        }
 
-        print("Part 2 (\(source)):")
+        let maxY = input.count
+        let maxX = input[0].count
+        var map: [Point: Int] = [:]
+        for y in 0 ..< maxY {
+            for x in 0 ..< maxX {
+                map[Point(x: x, y: y)] = input[y][x]
+            }
+        }
+
+        var lowestPoints: [Point] = []
+        for y in 0 ..< maxY {
+            for x in 0 ..< maxX {
+                let height = map[Point(x: x, y: y)]!
+                if height < map[Point(x: x-1, y: y), default: 10] &&
+                    height < map[Point(x: x, y: y - 1), default: 10] &&
+                    height < map[Point(x: x + 1, y: y), default: 10] &&
+                    height < map[Point(x: x, y: y + 1), default: 10] {
+                    lowestPoints.append(Point(x: x, y: y))
+                }
+            }
+        }
+
+        var basinSizes: [Int] = []
+        for lowPoint in lowestPoints {
+            var visited: [Point] = []
+            var queue = [lowPoint]
+            while !queue.isEmpty {
+                let p = queue.removeFirst()
+                visited.append(p)
+                for next in [p.up, p.left, p.right, p.down] {
+                    if !visited.contains(next) && !queue.contains(next) && map[next, default: 9] < 9 {
+                        queue.append(next)
+                    }
+                }
+            }
+            basinSizes.append(visited.count)
+        }
+        let product = basinSizes.sorted().suffix(3).reduce(1, *)
+
+        print("Part 2 (\(source)): \(product)")
     }
 }
 

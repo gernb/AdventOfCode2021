66309460b58871219f7ea0eda65999fa07228732
peter bohac <gern@1dot0.net>
2021-12-10 21:21:19 -0800

    Part 1

--- Day 11/InputData.swift
+++ Day 11/InputData.swift
@@ -14,9 +14,29 @@ enum InputData: String, CaseIterable {
         switch self {
 
         case .example: return """
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
 """.components(separatedBy: .newlines)
 
         case .challenge: return """
+7612648217
+7617237672
+2853871836
+7214367135
+1533365614
+6258172862
+5377675583
+5613268278
+8381134465
+3445428733
 """.components(separatedBy: .newlines)
 
         }
--- Day 11/main.swift
+++ Day 11/main.swift
@@ -9,11 +9,74 @@
 
 print("Day 11:")
 
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+
+    var left: Self { .init(x: x - 1, y: y) }
+    var up: Self { .init(x: x, y: y - 1) }
+    var right: Self { .init(x: x + 1, y: y) }
+    var down: Self { .init(x: x, y: y + 1) }
+    var upLeft: Self { .init(x: x - 1, y: y - 1) }
+    var upRight: Self { .init(x: x + 1, y: y - 1) }
+    var downLeft: Self { .init(x: x - 1, y: y + 1) }
+    var downRight: Self { .init(x: x + 1, y: y + 1) }
+
+    var adjacent: [Self] {
+        [upLeft, up, upRight, left, right, downLeft, down, downRight]
+    }
+}
+
+extension InputData {
+    func asMap() -> [Point: Int] {
+        var map: [Point: Int] = [:]
+        for y in 0 ..< self.data.count {
+            let line = Array(self.data[y])
+            for x in 0 ..< line.count {
+                let p = Point(x: x, y: y)
+                map[p] = Int(String(line[x]))
+            }
+        }
+        return map
+    }
+}
+
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        var map = source.asMap()
 
-        print("Part 1 (\(source)):")
+        var flashes = 0
+        for _ in 1 ... 100 {
+        // Increase each point by 1
+        for p in map.keys {
+            map[p]! += 1
+        }
+        // Flash anything greater than 9
+        var flashed: Set<Point> = []
+        var octopusFlashed = false
+        repeat {
+            octopusFlashed = false
+            for p in map.keys {
+                if map[p]! > 9 && !flashed.contains(p) {
+                    flashed.insert(p)
+                    octopusFlashed = true
+                    flashes += 1
+                    for adj in p.adjacent {
+                        guard let val = map[adj] else { continue }
+                        map[adj] = val + 1
+                    }
+                }
+            }
+        } while octopusFlashed
+        // Reset anything that flashed
+        for p in map.keys {
+            if map[p]! > 9 {
+                map[p]! = 0
+            }
+        }
+        }
+
+        print("Part 1 (\(source)): \(flashes)")
     }
 }
 

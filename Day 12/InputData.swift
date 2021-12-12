447a5f3faf4d8d5c32c6a1f66203b6998bfe4202
peter bohac <gern@1dot0.net>
2021-12-11 19:26:10 -1000

    Part 1

--- Day 12/InputData.swift
+++ Day 12/InputData.swift
@@ -8,15 +8,82 @@
 import Foundation
 
 enum InputData: String, CaseIterable {
-    case example, challenge
+    case example1, example2, example3, challenge
 
     var data: [String] {
         switch self {
 
-        case .example: return """
+        case .example1: return """
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+""".components(separatedBy: .newlines)
+
+        case .example2: return """
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+""".components(separatedBy: .newlines)
+
+        case .example3: return """
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
 """.components(separatedBy: .newlines)
 
         case .challenge: return """
+end-ry
+jf-jb
+jf-IO
+jb-hz
+jo-LM
+hw-end
+hw-LM
+hz-ry
+WI-start
+LM-start
+kd-jf
+xi-WI
+hw-jb
+hz-jf
+LM-jb
+jb-xi
+ry-jf
+WI-jb
+end-hz
+jo-start
+WI-jo
+xi-ry
+xi-LM
+xi-hw
+jo-xi
+WI-jf
 """.components(separatedBy: .newlines)
 
         }
--- Day 12/main.swift
+++ Day 12/main.swift
@@ -9,11 +9,66 @@
 
 print("Day 12:")
 
+final class Room {
+    let name: String
+    var connections: Set<Room> = []
+
+    var isSmallRoom: Bool {
+        name.lowercased() == name
+    }
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+extension Room: Equatable, Hashable {
+    static func == (lhs: Room, rhs: Room) -> Bool {
+        lhs.name == rhs.name
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+}
+
+extension InputData {
+    func asRooms() -> (start: Room, end: Room) {
+        var rooms: [String: Room] = [:]
+        for line in self.data {
+            let names = line.components(separatedBy: "-")
+            let left = rooms[names[0], default: Room(name: names[0])]
+            let right = rooms[names[1], default: Room(name: names[1])]
+            left.connections.insert(right)
+            right.connections.insert(left)
+            rooms[left.name] = left
+            rooms[right.name] = right
+        }
+        return (rooms["start"]!, rooms["end"]!)
+    }
+}
+
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let (start, end) = source.asRooms()
 
-        print("Part 1 (\(source)):")
+        var paths: [[Room]] = []
+        var queue = [[start]]
+        while !queue.isEmpty {
+            let path = queue.removeFirst()
+            let last = path.last!
+            if last == end {
+                paths.append(path)
+                continue
+            }
+            for next in last.connections {
+                if !next.isSmallRoom || !path.contains(next) {
+                    queue.append(path + [next])
+                }
+            }
+        }
+
+        print("Part 1 (\(source)): \(paths.count)")
     }
 }
 
